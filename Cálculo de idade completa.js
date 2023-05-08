e12d3bdd8b7ba642aca0493fc140db5c486f84c7
João Vitor Araújo de Jesus <joaovitoraj@poli.ufrj.br>
2023-05-07 21:06:17 -0300

    Cálculo de idade completa.js

---
+++ Cálculo de idade completa.js
@@ -0,0 +1,4 @@
+ var nascimento = new Date(prompt("Digite a data de nascimento (mm/dd/aaaa):"));
+ var hoje = new Date(prompt('Digite a data de hoje (mm/dd/aaaa): '));
+ var diferenca = parseInt((hoje.getTime() - nascimento.getTime())/(1000*3600*24*365));
+ alert(diferenca+' anos')
